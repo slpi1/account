63d5f8d0a2b9af2f83d1ad03305c1a050106dbea
雷行 <songzhp@youzu.com>
2021-01-15 16:14:13 +0800

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return file_get_contents(public_path('index.html'));
+        return file_get_contents(public_path('dist/index.html'));
     }
 }
